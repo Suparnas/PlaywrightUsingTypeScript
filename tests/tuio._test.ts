39533d48097b68003da923753f679522110a5cbb
Suparna <dsouza.supy14@gmail.com>
2025-03-20 01:51:59 -0400

    Update tuio._test.ts

--- tests/tuio._test.ts
+++ tests/tuio._test.ts
@@ -4,13 +4,19 @@ test.describe('test TUIO Create Transfer', () => {
 
 test('Create Transfer', async ({ page }) => {
   await page.goto('https://qa.interview.tuio.dev/?page=1&limit=25&sortName=id&sortDirection=desc');
-  await page.getByText('Create Transfer').click();
-  await expect(page.locator('.mat-dialog-title')).toHaveText("Add transfer");
-  await page.getByLabel('Details').fill('Monthly Subscription 1');
-  await page.getByLabel('price').fill('$10.00');
-  await page.locator('.mat-radio-container').nth(1).check();
+  await page.getByRole('button', { name: 'Create Transfer'}).click();
+  await page.getByRole('textbox', { name: 'Details' }).fill('Annual pro subscription fee');
+  await page.getByRole('textbox', { name: 'price' }).fill('$10.00');
+  //await page.getByRole('radio', { name: 'None - 0%' }).check();
+  //await (page.getByRole('radio', { name: 'sales-tax' }).nth(1)).click();
+  await (page.locator('.mat-radio-container').nth(1)).click(); //
+
+  
   await page.getByRole('button', { name: 'Add'}).click();
-  await expect(page.locator('td.left.fs-list-col.ng-star-inserted').nth(2)).toHaveText('Monthly Subscription 1');
-  await expect(page.locator('td.left.fs-list-col.ng-star-inserted').nth(1)).toHaveText(' Oct 16, 2024 ');
+ 
+  const firstRow = page.getByRole('row').nth(1); // nth(1) to skip the header row and select the first data row
+  await expect(firstRow.getByRole('gridcell').nth(0)).toHaveText('24');
+  await expect(firstRow.getByRole('gridcell').nth(1)).toHaveText('Mar 20, 2025');
+  await expect(firstRow.getByRole('gridcell').nth(2)).toHaveText('Annual pro subscription fee');
   });
 });
